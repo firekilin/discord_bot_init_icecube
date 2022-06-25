390b087c85ecdd485bc35ff396b6111f660358c4
firekilin <wl00161839@gmail.com>
2022-06-26 00:36:39 +0800

    test

--- app.js
+++ app.js
@@ -2,76 +2,49 @@ import 'dotenv/config';
 import express from 'express';
 import { InteractionType, InteractionResponseType,verifyKeyMiddleware  } from 'discord-interactions';
 import { VerifyDiscordRequest, DiscordRequest } from './utils.js';
-
+import {
+  CHALLENGE_COMMAND,
+  TEST_COMMAND,
+  HasGuildCommands,
+} from './commands.js';
 // Create and configure express app
 const app = express();
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
-  const message = req.body;
-  if (message.type === InteractionType.APPLICATION_COMMAND) {
-    if (data.name === 'test') {
-      // Send a message as response
+  // Interaction type and data
+  const { type, id, data } = req.body;
+
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" guild command
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: { content: 'A wild message appeared' },
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: 'hello world ' + getRandomEmoji(),
+        },
       });
     }
-    res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: 'Hello world',
-      },
-    });
   }
-  if (name === 'test') {
-    // Send a message into the channel where command was triggered from
-    return res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-        // Fetches a random emoji to send from a helper function
-        content: 'hello world ' + getRandomEmoji(),
-    },
-    });
-}
 });
 
-async function createCommand() {
-  const appId = process.env.APP_ID;
-  const guildId = process.env.GUILD_ID;
-
-  /**
-   * Globally-scoped slash commands (generally only recommended for production)
-   * See https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-   */
-  // const globalEndpoint = `applications/${appId}/commands`;
-
-  /**
-   * Guild-scoped slash commands
-   * See https://discord.com/developers/docs/interactions/application-commands#create-guild-application-command
-   */
-  const guildEndpoint = `applications/${appId}/guilds/${guildId}/commands`;
-  const commandBody = {
-    name: 'test',
-    description: 'Just your average command',
-    // chat command (see https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types)
-    type: 1,
-  };
-
-  try {
-    // Send HTTP request with bot token
-    const res = await DiscordRequest(guildEndpoint, {
-      method: 'POST',
-      body: commandBody,
-    });
-    console.log(await res.json());
-  } catch (err) {
-    console.error('Error installing commands: ', err);
-  }
-}
 
 app.listen(9527, () => {
   console.log('Listening on port 9527');
 
-  //createCommand();
+  // Check if guild commands from commands.json are installed (if not, install them)
+  HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
+    TEST_COMMAND,
+    CHALLENGE_COMMAND,
+  ]);
 });

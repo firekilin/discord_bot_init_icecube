eb6e9b0ff39e00b34dcb39c2f849836ed8e84797
firekilin <wl00161839@gmail.com>
2022-06-26 13:08:35 +0800

    test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
  app.post('/interactions', async function (req, res) {
   // Interaction type and data
   const { type, id, data } = req.body;
-
+  console.log(req.body);
   /**
    * Handle verification requests
    */
